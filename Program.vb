0acbc07ba981a0465c6a541200c75e17627d3857
Mahesa Fathir <148003046+thirrr@users.noreply.github.com>
2025-10-05 11:42:11 +0700

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,21 @@
+Module Program
+    Sub Main()
+
+        Dim nama As String = "Mahesa Fathir Muhammad"
+        Dim programStudi As String = "Teknik Informatika"
+        Dim kampus As String = "Universitas Pelita Bangsa"
+        Dim hobi As String = "Bermain Game"
+
+
+        Console.WriteLine()
+        Console.WriteLine("Hasil Output")
+        Console.WriteLine("Hallo, nama saya adalah " & nama)
+        Console.WriteLine("Saya kuliah pada jurusan " & programStudi & " di " & kampus)
+        Console.WriteLine("Saya memiliki hobi " & hobi)
+
+
+        Console.WriteLine()
+        Console.WriteLine("Tekan ENTER untuk keluar...")
+        Console.ReadLine()
+    End Sub
+End Module
